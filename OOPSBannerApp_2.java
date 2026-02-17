76768b4a3bb22d53cff43cd40186986a391e8271
toshitaaashish-rgb <toshitaaashish@gmail.com>
2026-02-17 10:47:33 +0530

    Coded to Render Banner using Print Statements

---
+++ OOPSBannerApp_2.java
@@ -0,0 +1,20 @@
+/**
+ * @author: Toshitaa
+ * @version: 1.0
+ */
+
+public class OOPSBannerApp_2 {
+    public static void main(String[] args) {
+
+        System.out.println(" *****     *****    ******     *****  ");
+        System.out.println("*     *   *     *   *     *   *     * ");
+        System.out.println("*     *   *     *   *     *   *       ");
+        System.out.println("*     *   *     *   ******     *****  ");
+        System.out.println("*     *   *     *   *              *  ");
+        System.out.println("*     *   *     *   *        *     *  ");
+        System.out.println(" *****     *****    *         *****   ");
+
+    }
+}
+
+
