5f1176ef49afe7f4e14aeea0c7cf7860cab3a7bc
toshitaaashish-rgb <toshitaaashish@gmail.com>
2026-03-26 10:55:05 +0530

    Add files via upload

---
+++ OOPSBannerApp_8.java
@@ -0,0 +1,55 @@
+/*
+*@author: Toshitaa 
+*
+*@version: v1.0
+*/
+import java.util.HashMap;
+
+public class OOPSBannerApp_8 {
+
+    static HashMap<Character, String[]> letters = new HashMap<>();
+
+    static {
+        letters.put('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        });
+
+        letters.put('P', new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    "
+        });
+
+        letters.put('S', new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        });
+    }
+
+    public static void render(String text) {
+        text = text.toUpperCase();
+
+        for (int row = 0; row < 5; row++) {
+            for (char c : text.toCharArray()) {
+                String[] pattern = letters.get(c);
+                if (pattern != null) {
+                    System.out.print(pattern[row] + "  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        render("OOPS");
+    }
+}
